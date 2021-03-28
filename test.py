fad07e1ed0cd8d74dead31f954d387c272aca807
Kabir Gupta <kabirg108@gmail.com>
2021-03-27 21:20:04 -0500

    moved people to another script and yay Stuff Works TM

--- test.py
+++ test.py
@@ -1,4 +1,29 @@
 from pandas import *
+students=read_csv("Students.csv")
+families=read_csv("Family Members.csv")
+
+from people import *
+
+
+# MAKE THIS WORK
+"""
+for x in range(0,students.shape[0]):
+    student_x=student(x)
+"""
+
+student0=student(0)
+family0=family(0)
+
+student0.display()
+family0.display()
+
+
+
+
+
+
+
+
 
 # general function to create a df from a csv and read it in two ways
 def read_df(csv_filename,type):
@@ -14,57 +39,3 @@ def read_df(csv_filename,type):
                 print("%s: "%(column)+str(responses.at[row,column]))
             print("\n")
 
-students=read_csv("Students.csv")
-family=read_csv("Family Members.csv")
-
-class student:
-    last=""
-    first=""
-    genres=""
-    m1=0
-    m2=0
-    m3=0
-    m4=0
-    m5=0
-    m6=0
-    m7=0
-    m8=0
-    m9=0
-    m10=0
-    m11=0
-    m12=0
-    fam1last=""
-    fam1first=""
-    fam2last=""
-    fam2first=""
-    friend1last=""
-    friend2last=""
-    friend2first=""
-
-    def __init__(self, row):
-        self.last=students.iat[row,0]
-        self.first=students.iat[row,1]
-        self.genres=students.iat[row,2]
-        self.m1=students.iat[row,3]
-        self.m2=students.iat[row,4]
-        self.m3=students.iat[row,5]
-        self.m4=students.iat[row,6]
-        self.m5=students.iat[row,7]
-        self.m6=students.iat[row,8]
-        self.m7=students.iat[row,9]
-        self.m8=students.iat[row,10]
-        self.m9=students.iat[row,11]
-        self.m10=students.iat[row,12]
-        self.m11=students.iat[row,13]
-        self.m12=students.iat[row,14]
-        self.fam1last=students.iat[row,15]
-        self.fam1first=students.iat[row,16]
-        self.fam2last=students.iat[row,17]
-        self.fam2first=students.iat[row,18]
-        self.friend1last=students.iat[row,19]
-        self.friend1first=students.iat[row,20]
-        self.friend2last=students.iat[row,21]
-        self.friend2first=students.iat[row,22]
-
-for x in range(0,students.shape[0]):
-    student_x=student(x)
