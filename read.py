fe75a8075c48deacb9c740fb142828fd59cf23ff
Kabir Gupta <kabirg108@gmail.com>
2021-03-27 20:00:36 -0500

    read.py is ready

--- read.py
+++ read.py
@@ -1,3 +1,13 @@
 import pandas as d
-responses = d.read_csv('responses.csv')
+print("Enter the filename for a CSV: ")
+filename=input()
+responses=d.read_csv(filename)
 
+print("Displayed as Pandas dataframe:")
+print(responses)
+print("\nDisplayed with loops :)\n")
+for row in range(1,19):
+    print("Student #%s:"%(row))
+    for column in responses:
+        print("%s: "%(column)+str(responses.at[row,column]))
+    print("\n")
