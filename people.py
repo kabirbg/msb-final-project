b7355f1c20f7ffe6a8cd015c595f54edd8f12549
Kabir Gupta <kabirg108@gmail.com>
2021-05-09 16:12:36 -0500

    people.py bugless! hopefully ✨

--- people.py
+++ people.py
@@ -8,11 +8,11 @@ class student:#I'll have one object of this type for each participating student
     name=[]#holds first and last name
     fam=[]#holds family members' names
     friends=[]#holds friends' names
-    rank=[-1,-1,-1,-1,-1,-1]#holds ordinal rankings for each genre
+    ranks=[-1,-1,-1,-1,-1,-1]#holds ordinal rankings for each genre
 
     def display(self):
         print(self.name)
-        print(self.rank)
+        print(self.ranks)
         print(self.fam)
         print(self.friends)
         print("\n")
@@ -20,37 +20,36 @@ class student:#I'll have one object of this type for each participating student
     def __init__(self, row):
         #basic info
         self.name=[students.iat[row,0],students.iat[row,1]] #name (last,first) is a list
-        self.fam=[(students.iat[row,15],students.iat[row,15]),(students.iat[row,17],students.iat[row,18])]#each family member occupies a list similar to above; fam contains both lists
+        self.fam=[(students.iat[row,15],students.iat[row,16]),(students.iat[row,17],students.iat[row,18])]#each family member occupies a list similar to above; fam contains both lists
         self.friends=[(students.iat[row,19],students.iat[row,20]),(students.iat[row,21],students.iat[row,22])]#same as above, for friends
         #generate & store rankings
         genres=students.iat[row,2]#temporarily store genre selections
         musics=[students.iat[row,3],students.iat[row,4],students.iat[row,5],students.iat[row,6],students.iat[row,7],students.iat[row,8],students.iat[row,9],students.iat[row,10],students.iat[row,11],students.iat[row,12],students.iat[row,13],students.iat[row,14]]#temporarily store song selections
-        print(musics)
         for genre in range(6): #each number represents a genre (1=pop, 2=jazz & blues, and so on)
             #assign rank system based on flowchart:
             if str(genre+1) in genres: #liked the genre
                 if musics[genre*2-1]==1 and musics[genre*2]==1: #liked both musics
-                    self.rank[genre-1]=5
+                    self.ranks[genre-1]=5
                 elif musics[genre*2-1]==1 or musics[genre*2]==1: #liked one music
-                    self.rank[genre-1]=4
+                    self.ranks[genre-1]=4
                 else: #liked none
-                    self.rank[genre-1]=3
+                    self.ranks[genre-1]=3
             else: #disliked the genre
                 if musics[genre*2-1]==1 and musics[genre*2]==1: #liked both musics
-                    self.rank[genre-1]=2
+                    self.ranks[genre-1]=2
                 elif musics[genre*2-1]==1 or musics[genre*2]==1: #liked one music
-                    self.rank[genre-1]=1
+                    self.ranks[genre-1]=1
                 else: #liked none
-                    self.rank[genre-1]=0
+                    self.ranks[genre-1]=0
         self.display()#display info to ensure operation completed successfully
 
 class family:#I'll have one object of this type for each participating family member; identical to Student but lacks fam[] and friends[]
     name=[]
-    rank=[-1,-1,-1,-1,-1,-1]
+    ranks=[-1,-1,-1,-1,-1,-1]
 
     def display(self):
         print(self.name)
-        print(self.rank)
+        print(self.ranks)
         print("\n")
 
     def __init__(self, row):
@@ -60,16 +59,16 @@ class family:#I'll have one object of this type for each participating family me
         for genre in range(6):
             if str(genre) in genres:
                 if musics[genre*2-1]==1 and musics[genre*2]==1:
-                    self.rank[genre-1]=5
+                    self.ranks[genre-1]=5
                 elif musics[genre*2-1]==1 or musics[genre*2]==1:
-                    self.rank[genre-1]=4
+                    self.ranks[genre-1]=4
                 else:
-                    self.rank[genre-1]=3
+                    self.ranks[genre-1]=3
             else:
                 if musics[genre*2-1]==1 and musics[genre*2]==1:
-                    self.rank[genre-1]=2
+                    self.ranks[genre-1]=2
                 elif musics[genre*2-1]==1 or musics[genre*2]==1:
-                    self.rank[genre-1]=1
+                    self.ranks[genre-1]=1
                 else:
-                    self.rank[genre-1]=0
+                    self.ranks[genre-1]=0
         self.display()
