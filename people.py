897d061454976d1736fade8948b7b7105c0eb1c4
Kabir Gupta <kabirg108@gmail.com>
2021-05-20 13:11:22 -0500

    fixed formatting

--- people.py
+++ people.py
@@ -5,9 +5,18 @@ from pandas import read_csv  # necessary for importing csv data as a pandas data
 students = read_csv("private/Students.csv")  # initialize dataframe for students
 families = read_csv("private/Family.csv")  # and one for families
 
+
 class Student:
     def __str__(self):
-        return str(self.name) + "\n" + str(self.ranks) + "\n" + str(self.fam) + "\n" + str(self.friends)
+        return (
+            str(self.name)
+            + "\n"
+            + str(self.ranks)
+            + "\n"
+            + str(self.fam)
+            + "\n"
+            + str(self.friends)
+        )
 
     @classmethod
     def at_row(cls, n: int):
@@ -18,7 +27,7 @@ class Student:
     # out in (first name, last name, ...  musics ..., family member a first name,
     # family member a last name, family number b first name, family number b
     # last name, friend a first name, friend a last name, friend b first name,
-    # friend b last name.
+    # friend b last name).
     def __init__(self, row):
         self.name = (
             row[0],
@@ -44,43 +53,29 @@ class Student:
         # Each genre is represented by a number (1=pop, 2=jazz & blues, etc).
         for genre in range(1, 7):
             if str(genre) in genres:  # liked the genre
-                self.ranks[genre - 1] = 3 \
-                                      + musics[genre * 2 - 2] \
-                                      + musics[genre * 2 - 1]
+                self.ranks[genre - 1] = (
+                    3 + musics[genre * 2 - 2] + musics[genre * 2 - 1]
+                )
             else:  # disliked the genre
-                self.ranks[genre - 1] = musics[genre * 2 - 2] \
-                                      + musics[genre * 2 - 1]
+                self.ranks[genre - 1] = musics[genre * 2 - 2] + musics[genre * 2 - 1]
 
 
 class Family:  # I'll have one object of this type for each participating family member; identical to Student but lacks fam[] and friends[]
     def __str__(self):
-        return str(self.name) + "\n" + str(self.ranks) + "\n" + str(self.fam) + "\n" + str(self.friends)
+        return str(self.name) + "\n" + str(self.ranks)
 
     @classmethod
     def at_row(cls, n: int):
-        row = [student for student in students.iloc[n]]
+        row = [family for family in families.iloc[n]]
         return cls(row)
 
-    # Construct a Student from a row of data. This row is expected to be laid
-    # out in (first name, last name, ...  musics ..., family member a first name,
-    # family member a last name, family number b first name, family number b
-    # last name, friend a first name, friend a last name, friend b first name,
-    # friend b last name.
+    # Construct a Family from a row of data. This row is expected to be laid
+    # out in (first name, last name, genres,  musics ...).
     def __init__(self, row):
         self.name = (
             row[0],
             row[1],
         )
-
-        self.fam = [
-            (row[15], row[16]),
-            (row[17], row[18]),
-        ]
-
-        self.friends = [
-            (row[19], row[20]),
-            (row[21], row[22]),
-        ]
 
         self.ranks = [-1 for n in range(1, 7)]
 
@@ -91,9 +86,8 @@ class Family:  # I'll have one object of this type for each participating family
         # Each genre is represented by a number (1=pop, 2=jazz & blues, etc).
         for genre in range(1, 7):
             if str(genre) in genres:  # liked the genre
-                self.ranks[genre - 1] = 3 \
-                                      + musics[genre * 2 - 2] \
-                                      + musics[genre * 2 - 1]
+                self.ranks[genre - 1] = (
+                    3 + musics[genre * 2 - 2] + musics[genre * 2 - 1]
+                )
             else:  # disliked the genre
-                self.ranks[genre - 1] = musics[genre * 2 - 2] \
-                                      + musics[genre * 2 - 1]
+                self.ranks[genre - 1] = musics[genre * 2 - 2] + musics[genre * 2 - 1]
